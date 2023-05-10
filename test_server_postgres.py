3f07fa8e9dd8c6c85557e6a4dd8d623bf7741ac0
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-05-10 13:52:46 -0400

    update db

--- test_server_postgres.py
+++ test_server_postgres.py
@@ -6,7 +6,6 @@ import jwt
 import psycopg2
 
 app = Tangerine()
-
 keychain = KeyLime({
         "SECRET_KEY": "ILOVECATS",
 })
@@ -14,10 +13,8 @@ keychain = KeyLime({
 def get_user_by_email(email):
     conn = psycopg2.connect("postgresql://postgres:C4melz!!@localhost:5432/local_development")
     cur = conn.cursor()
-
     cur.execute("SELECT * FROM tangerine.users WHERE email = %s", (email,))
     user = cur.fetchone()
-
     cur.close()
     conn.close()
 
@@ -29,14 +26,11 @@ def get_user_by_email(email):
 def create_user(user_data):
     conn = psycopg2.connect("postgresql://postgres:C4melz!!@localhost:5432/local_development")
     cur = conn.cursor()
-
     cur.execute("INSERT INTO tangerine.users (email, password) VALUES (%s, %s) RETURNING id", (user_data['email'], user_data['password']))
     user_id = cur.fetchone()[0]
-
     conn.commit()
     cur.close()
     conn.close()
-
     return {'_id': user_id, 'email': user_data['email'], 'password': user_data['password']}
 
 auth = Yuzu(keychain, get_user_by_email, create_user)
@@ -71,11 +65,17 @@ def logout(ctx: Ctx) -> None:
     ctx.body = json.dumps({"message": "Logged out successfully"})
     ctx.send(200, content_type='application/json')
 
+@Router.auth_required
+def get_protected_content(ctx: Ctx) -> None:
+    ctx.body = json.dumps({"message": "This is protected content. Only authenticated users can see this. I hope you feel special 🍊🍊🍊."})
+    ctx.send(200, content_type='application/json')
+
 # ==================== API ROUTES ====================
 api_router = Router(prefix='/api')
 api_router.post('/logout', logout)
 api_router.post('/login', login)
 api_router.post('/signup', signup)
+api_router.get('/protected', get_protected_content)
 
 app.use(auth.jwt_middleware)
 app.use_router(api_router)
