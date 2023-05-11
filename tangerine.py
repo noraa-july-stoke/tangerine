91ccb7ea977100174e7cf70be1ecb01bcc319f59
Korbin Allseitz <42754870+kallseit@users.noreply.github.com>
2023-05-10 22:23:27 -0700

    📄 - tangerine/tangerine.py: Add Doc Strings

--- tangerine.py
+++ tangerine.py
@@ -32,6 +32,22 @@ T = TypeVar("T")
 logging.basicConfig(level=logging.DEBUG)
 
 class Tangerine:
+    """
+    Initializes the Tangerine application. This includes creating the server socket and setting the host, port, and routes.
+
+    Attributes:
+    host (str): The host for the server.
+    port (int): The port for the server.
+    routes (dict): The routes for the application.
+    middlewares (list): A list of middleware functions.
+    debug_level (int): The level of debugging information to display.
+
+    Parameters:
+    host (str): The host for the server. Defaults to 'localhost'.
+    port (int): The port for the server. Defaults to 8000.
+    debug_level (int): The level of debugging information to display. Defaults to 0.
+    """
+    
     def __init__(self: T, host: str = 'localhost', port: int = 8000, debug_level: int = 0) -> None:
         self.host: str = host
         self.port: int = port
@@ -52,6 +68,15 @@ class Tangerine:
 
 
     def debugger(self, middleware: Callable[[Ctx], None]) -> Callable[[Ctx], None]:
+        """
+        Wraps a middleware function for debugging. If the debug level is set to a value greater than 0, it will print the state of the context before and after the middleware is applied.
+
+        Parameters:
+        middleware (Callable[[Ctx], None]): The middleware function to wrap.
+
+        Returns:
+        Callable[[Ctx], None]: The wrapped middleware function.
+        """
         def wrapper(ctx: Ctx) -> None:
             if self.debug_level > 0:
                 old_state = ctx.to_dict()
@@ -81,9 +106,18 @@ class Tangerine:
         return wrapper
 
     def use(self, middleware: Callable[[Request, Response], None]) -> None:
+        """
+        Adds a middleware function to the application.
+
+        Parameters:
+        middleware (Callable[[Request, Response], None]): The middleware function to add.
+        """
         self.middlewares.append(middleware)
 
     def _create_socket(self):
+        """
+        Creates the server socket and sets the appropriate options.
+        """
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(
             socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -91,17 +125,37 @@ class Tangerine:
         self.server_socket.listen()
 
     def register_route(self, method, path, handler):
+        """
+        Registers a route for the application.
+
+        Parameters:
+        method (str): The HTTP method for the route (e.g., 'GET', 'POST', etc.).
+        path (str): The path for the route.
+        handler (Callable): The function to handle requests to the route.
+        """
         if method not in self.routes:
             self.routes[method] = {}
         self.routes[method][path] = handler
 
 
     def use_router(self: T, router: Router) -> None:
+        """
+        Adds a router to the application.
+
+        Parameters:
+        router (Router): The router to add to the application.
+        """
         router.set_debug_level(self.debug_level)
         self.routers[router.prefix] = router
 
-    # serve static files from route pattern and folder directory specified by user
     def static(self: T, route_pattern: str, dir_path: str):
+        """
+        Serves static files from the specified folder directory using the given route pattern.
+
+        Parameters:
+        route_pattern (str): The route pattern to match for static files.
+        dir_path (str): The folder directory to serve static files from.
+        """
         self.static_route_pattern = route_pattern
         self.static_dir_path = dir_path
         # Use regular expressions to match the requested path against the static route pattern
@@ -109,6 +163,15 @@ class Tangerine:
 
 
     def parse_request(self, request: bytes) -> Tuple[str, str, Dict[str, str], Union[str, bytes]]:
+        """
+        Parses an incoming HTTP request and returns the method, path, headers, and body.
+
+        Parameters:
+        request (bytes): The raw HTTP request data.
+
+        Returns:
+        Tuple[str, str, Dict[str, str], Union[str, bytes]]: A tuple containing the method, path, headers, and body.
+        """
         # Split the request into its individual lines
         lines = request.decode('utf-8').split('\r\n')
 
@@ -137,9 +200,24 @@ class Tangerine:
 
 
     def handle_new_client(self, client_socket: socket.socket, inputs: List[socket.socket]) -> None:
+        """
+        Handles a new client connection by adding the client's socket to the inputs list.
+
+        Parameters:
+        client_socket (socket.socket): The client's socket.
+        inputs (List[socket.socket]): The list of input sockets.
+        """
         inputs.append(client_socket)
 
     def handle_existing_client(self, sock: socket.socket, inputs: List[socket.socket], outputs: List[socket.socket]) -> None:
+        """
+        Handles an existing client connection by processing the client's request and sending a response.
+
+        Parameters:
+        sock (socket.socket): The client's socket.
+        inputs (List[socket.socket]): The list of input sockets.
+        outputs (List[socket.socket]): The list of output sockets.
+        """
         request = sock.recv(4096)
         if request:
             # Parse the HTTP request and extract the method, path, and headers
@@ -189,6 +267,13 @@ class Tangerine:
             inputs.remove(sock)
 
     def handle_static_route(self, ctx: Ctx, path: str) -> None:
+        """
+        Handles a static route by serving a static file, if it exists.
+
+        Parameters:
+        ctx (Ctx): The context for the request and response.
+        path (str): The requested path.
+        """
         file_path: str = os.path.join(self.static_dir_path, path.lstrip('/'))
 
         if not os.path.exists(file_path) or not os.path.isfile(file_path):
@@ -201,10 +286,19 @@ class Tangerine:
             ctx.send(200, data, content_type=content_type)
 
     def handle_not_found(self, ctx: Ctx) -> None:
+        """
+        Handles a 404 Not Found error by setting the appropriate status code and headers in the response.
+
+        Parameters:
+        ctx (Ctx): The context for the request and response.
+        """
         ctx.send(404, b'404 Not Found')
         ctx.set_res_header('Content-Type', 'text/plain')
 
     def run(self) -> None:
+        """
+        Starts the server and processes incoming client connections.
+        """
         inputs = [self.server_socket]
         outputs = []
         print_success(self.port, self.host, self.debug_level)
@@ -222,6 +316,9 @@ class Tangerine:
                     self.handle_existing_client(sock, inputs, outputs)
 
     def start(self: T) -> None:
+        """
+        Starts the server and handles KeyboardInterrupt and server socket closure.
+        """
         try:
             self.run()
         except KeyboardInterrupt:
