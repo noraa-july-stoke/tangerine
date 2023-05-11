19f07079a144be31589092b947edaba84748543b
Korbin Allseitz <42754870+kallseit@users.noreply.github.com>
2023-05-10 22:23:14 -0700

    📄 - tangerine/response.py: Add Doc Strings

--- response.py
+++ response.py
@@ -77,31 +77,66 @@ STATUS_CODES = {
 
 class Response:
     """
-        The Response class is the class that is stores in the context object.
-        It provides a way to construct the response that will be sent to the client.
+    The Response class is responsible for storing HTTP response data including the status code, headers, and body.
+    This class provides methods to manipulate these response properties and render them into a proper HTTP response string.
+
+    Attributes:
+        status_code (int): The HTTP status code for the response.
+        headers (Dict[str, str]): The headers for the response.
+        body (str): The body of the response.
     """
+
     def __init__(self, status_code=404, headers=None, body=''):
-        # Set the status code, headers, and body of the response.
+        """
+        Initializes a new instance of the Response class.
+
+        Args:
+            status_code (int, optional): The HTTP status code for the response. Defaults to 404.
+            headers (Dict[str, str], optional): The headers for the response. Defaults to an empty dictionary.
+            body (str, optional): The body of the response. Defaults to an empty string.
+        """        
         self.status_code = status_code
         self.headers = headers or {}
         self.body = body
 
     def set_header(self, name, value):
-        # Set a header on the response.
+        """
+        Sets a header on the response.
+
+        Args:
+            name (str): The name of the header.
+            value (str): The value of the header.
+        """
         self.headers[name] = value
 
     def set_body(self, body):
-        # Set the body of the response.
+        """
+        Sets the body of the response.
+
+        Args:
+            body (str): The body of the response.
+        """
         self.body = body
 
     def render(self):
-        # Render the response into an HTTP response string.
-        # This function converts the response into a string that is sent to the client.
+        """
+        Renders the response into an HTTP response string. This method converts the response properties into a valid
+        HTTP response string that can be sent to the client.
+
+        Returns:
+            str: A string representation of the HTTP response.
+        """
         # The string is a valid HTTP response.
         headers = '\r\n'.join([f'{name}: {value}' for name, value in self.headers.items()])
         return f'HTTP/1.1 {self.status_code}\r\n{headers}\r\n\r\n{self.body}'
 
     def to_bytes(self: T) -> bytes:
+        """
+        Converts the HTTP response string to bytes.
+
+        Returns:
+            bytes: The byte representation of the HTTP response.
+        """
         status_line = f'HTTP/1.1 {self.status_code} {STATUS_CODES[self.status_code]}'
         # set key value pairs for headers
         headers = '\r\n'.join([f'{k}: {v}' for k, v in self.headers.items()])
@@ -112,16 +147,21 @@ class Response:
 
 
     def send(self, conn):
-        # Send the response to the client.
-        # This function sends the response to the client.
+        """
+        Sends the response to the client. This method converts the response into bytes and sends it to the client
+        through the given connection.
+
+        Args:
+            conn: The connection to the client.
+        """
         conn.sendall(self.to_bytes())
 
     def to_dict(self) -> dict:
         """
-        Creates dictionary representation of the Response Class
+        Creates a dictionary representation of the Response object.
 
         Returns:
-            dict: _description_
+            dict: A dictionary representation of the Response object. Includes 'status_code', 'headers', and 'body'.
         """
         try:
             body_json = json.loads(self.body)
@@ -134,4 +174,10 @@ class Response:
         }
 
     def __repr__(self):
+        """
+        Returns a string representation of the Response object, showing the status code.
+
+        Returns:
+            str: A string representation of the Response object.
+        """
         return f'<Response: {self.status_code}>'
