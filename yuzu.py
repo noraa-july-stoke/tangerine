10a5a5e02cc94883b429076dcaa49916c1ba0cd1
Korbin Allseitz <42754870+kallseit@users.noreply.github.com>
2023-05-11 17:30:13 -0700

    Revert tangerine and yuzu changes

--- yuzu.py
+++ yuzu.py
@@ -12,70 +12,28 @@ import jwt
 from typing import Dict, Tuple
 import json
 
-
 class Yuzu:
-    """
-    This class is responsible for handling user authentication, such as
-    logging in, logging out, signing up, and generating/verifying tokens.
-
-    Attributes:
-    keychain (KeyLime): An instance of KeyLime class, used for retrieving keys.
-    client: Database client used for user authentication.
-    auth (bool): Authentication status of the user.
-    user: Data of the authenticated user.
-    """
-    def __init__(self, keychain: KeyLime, client):
-        """
-        Parameters:
-        keychain (KeyLime): An instance of KeyLime class, used for retrieving keys.
-        client: Database client used for user authentication.
-        """
+    def __init__(self, keychain: KeyLime, get_user_by_email, create_user):
         self.keychain = keychain
-        self.client = client
+        self.get_user_by_email = get_user_by_email
+        self.create_user = create_user
         self.auth = False
         self.user = None
 
     def get_config(self, key_name: str) -> str:
-        """
-        Returns the value of the specified key from the keychain.
-
-        Parameters:
-        key_name (str): The name of the key.
-
-        Returns:
-        str: The value of the key.
-        """
         return self.keychain.get_key(key_name)
 
     def setup_database(self):
-        """
-        Sets up the database using the database connection string.
-        """
+        # Set up the database using self.db_connection_string
         pass
 
     def setup_other_configs(self):
-        """
-        Sets up other configurations using the Google Cloud or other configs from the keychain.
-        """
+        # Set up other configurations using self.google_cloud or other configs from the keychain
         pass
 
     def authenticate(self, email: str, password: str) -> bool:
-        """
-        Authenticates a user with the provided email and password.
-
-        Parameters:
-        email (str): The user's email.
-        password (str): The user's password.
-
-        Returns:
-        bool: True if the user is authenticated, False otherwise.
-        """
         try:
-            db = self.client['mydatabase']
-            users = db['users']
-
-            query = {'email': email}
-            user = users.find_one(query)
+            user = self.get_user_by_email(email)
 
             if user and bcrypt.checkpw(password.encode(), user['password'].encode()):
                 return True
@@ -87,17 +45,7 @@ class Yuzu:
             return False
 
     def generate_auth_token(self, user_id: str, email: str) -> str:
-        """
-        Generates an authentication token for a user.
-
-        Parameters:
-        user_id (str): The user's ID.
-        email (str): The user's email.
-
-        Returns:
-        str: The authentication token.
-        """
-        secret_key = self.get_config('JWT')["SECRET_KEY"]
+        secret_key = self.get_config("SECRET_KEY")
         print("GENERATE AUTH TOKEN VARIABLES", user_id, email, secret_key)
         token_payload = {
             "user_id": user_id,
@@ -106,18 +54,10 @@ class Yuzu:
         }
         return jwt.encode(token_payload, secret_key, algorithm='HS256')
 
+
     def verify_auth_token(self, token: str) -> dict:
-        """
-        Verifies an authentication token.
-
-        Parameters:
-        token (str): The authentication token.
-
-        Returns:
-        dict: The decoded token if it's valid and not expired, None otherwise.
-        """
         try:
-            secret_key = self.get_config('JWT')["SECRET_KEY"]
+            secret_key = self.get_config('SECRET_KEY')
             decoded_token = jwt.decode(token, secret_key, algorithms=['HS256'])
             return decoded_token
         except jwt.ExpiredSignatureError:
@@ -127,83 +67,40 @@ class Yuzu:
             print("Invalid token")
             return None
 
-    def jwt_middleware(self, ctx) -> None:
-        """
-        Middleware to handle JWT authentication.
+    def jwt_middleware(self, ctx, next) -> None:
+        # Extract the token from the request headers or cookies
+        token = ctx.get_req_header("Authorization")
+        user = None
+        # Verify the token and get the user
+        if token:
+            user = self.verify_auth_token(token)
+            ctx.auth = {"user": user}
+            next()  # Call next() only if the user is authenticated
 
-        Parameters:
-        ctx: The context for the request and response.
-        """
-        jwt_config = self.get_config("JWT")
-        protected_prefixes = jwt_config["PROTECTED_PREFIXES"]
-        bypass_allowed = jwt_config["BYPASS_ALLOWED"]
 
-        request_path = ctx.request.path
-        if any(request_path.startswith(prefix) for prefix in protected_prefixes) and request_path not in bypass_allowed:
-            token = ctx.get_req_header("Authorization")
-            print(token, "AUTH HEADER AUTH MIDDLE")
-
-            if not token:
-                ctx.body = json.dumps({"message": "Missing token"})
-                ctx.send(401, content_type='application/json')
-                return
-
-            decoded_token = self.verify_auth_token(token)
-            if decoded_token:
-                ctx.auth = {
-                    'user': decoded_token
-                }
-            else:
-                ctx.body = json.dumps({"message": "Invalid token"})
-                ctx.send(401, content_type='application/json')
-
-    def sign_up(self,user_data: dict) -> dict:
-        """
-        Signs up a new user.
-
-        Parameters:
-        user_data (dict): The user's data.
-
-        Returns:
-        dict: The created user's data, None if an error occurred.
-        """
+    def sign_up(self, user_data: dict) -> dict:
         try:
             hashed = bcrypt.hashpw(user_data['password'].encode(), bcrypt.gensalt())
             user_data['password'] = hashed.decode()  # Convert bytes to string
 
-            db = self.client['mydatabase']
-            users = db['users']
-            result = users.insert_one(user_data)
+            created_user = self.create_user(user_data)
 
-            print(f'User created with id: {result.inserted_id}')
-            user_data['_id'] = str(result.inserted_id)  # Convert ObjectId to string
-            return user_data
+            if created_user:
+                print(f'User created with id: {created_user["_id"]}')
+                return created_user
+            else:
+                return None
 
         except Exception as e:
             print(f'Error creating user: {e}')
             return None
 
 
-
     def login(self, email: str, password: str) -> Tuple[str, str]:
-        """
-        Logs in a user with the provided email and password.
-
-        Parameters:
-        email (str): The user's email.
-        password (str): The user's password.
-
-        Returns:
-        Tuple[str, str]: The user's ID and authentication token, (None, None) if an error occurred or the authentication failed.
-        """
         print(self.authenticate(email, password))
         if self.authenticate(email, password):
             try:
-                db = self.client['mydatabase']
-                users = db['users']
-
-                query = {'email': email}
-                user_data = users.find_one(query)
+                user_data = self.get_user_by_email(email)
                 print(user_data)
 
                 if user_data:
@@ -211,7 +108,6 @@ class Yuzu:
                     self.user = user_data
                     print(self.user, "SELF.USER")
                     token = self.generate_auth_token(str(user_data["_id"]), email)
-                    print(token, "TOKEN!!!!!!!!!!!!!")
                     return str(user_data["_id"]), token
                 else:
                     return None, None
@@ -222,9 +118,7 @@ class Yuzu:
         else:
             return None, None
 
+
     def logout(self):
-        """
-        Logs out the current user.
-        """
         self.auth = False
         self.user = None
